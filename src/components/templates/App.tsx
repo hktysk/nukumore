c14d3c6e8d0c5c44249ad300713873e8674d3646
hkt <komikansougou@gmail.com>
2020-03-10 16:55:41 +0900

    feat: おすすめ記事を3つ表示する

--- src/components/templates/App.tsx
+++ src/components/templates/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Recommend } from '../../types';
 import Header from '../organisms/Header';
 import CatchPhrase from '../molecules/CatchPhrase';
+import RecommendCards from '../organisms/RecommendCards';
 import './App.sass';
 
-const App: React.FC = () => (
+type Props = {
+  recommends: Recommend[]
+}
+
+const App: React.FC<Props> = ({ recommends }) => (
   <div className="App">
     <Header />
     <div className="App__catchPhrase">
       <CatchPhrase />
+    </div>
+    <div className="App__recommends">
+      <RecommendCards data={recommends} />
     </div>
   </div>
 );
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import recommends from '../components/atoms/recommend-data.json';
 import Template from '../components/templates/App';
 
 const App:React.FC = () => (
-  <Template />
+  <Template recommends={recommends} />
 );
 
 export default App;
