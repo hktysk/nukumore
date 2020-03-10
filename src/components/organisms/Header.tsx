8b530bf0105119fa93caf793dfbacb0abb012429
hkt <komikansougou@gmail.com>
2020-03-10 15:28:12 +0900

    feat: 検索枠を配置

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import SearchForm from './SearchForm';
 import './Header.sass';
 import Logo from '../atoms/logo.png';
 
 const Header: React.FC = () => (
   <header className="Header">
+    <div className="Header__searchForm">
+      <SearchForm />
+    </div>
     <img className="Header__logo" src={Logo} />
   </header>
 );
