af698386a54c697049be3ee26a2e3fe41f799225
hkt <komikansougou@gmail.com>
2020-03-10 15:52:13 +0900

    feat: キャッチフレーズを配置

---
+++ src/components/molecules/CatchPhrase.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CatchPhrase.sass';
+
+const CatchPhrase: React.FC = () => (
+  <div className="CatchPhrase">
+    ぬくもりは、ハンドメイド・手作り作品の作り方をご紹介するサイトです。子どもや初心者さん向けの簡単でかわいいものから、上級者向けのレシピまで取り揃えています！
+  </div>
+);
+
+export default CatchPhrase;
--- src/components/templates/App.tsx
+++ src/components/templates/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../organisms/Header';
+import CatchPhrase from '../molecules/CatchPhrase';
 import './App.sass';
 
 const App: React.FC = () => (
-  <div>
+  <div className="App">
     <Header />
+    <div className="App__catchPhrase">
+      <CatchPhrase />
+    </div>
   </div>
 );
 
